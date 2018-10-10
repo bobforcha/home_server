0f40eb385b65203a1dbb71821b539b822d6d2da6
Bob Forcha <bobforcha@gmail.com>
2018-10-10 17:21:35 -0400

    First commit
     - generate framework
     - use RSpec
     - setup Capybara with Selenium and Chromedriver

---
+++ spec/support/init/selenium_webdriver.rb
@@ -0,0 +1,11 @@
+require 'selenium/webdriver'
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(
+    args = %w[headless disable-gpu]
+  )
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :selenium_chrome_headless
