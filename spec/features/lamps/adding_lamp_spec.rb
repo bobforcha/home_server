e59f501d2cea72349c1e4adc9f26b333c7a6dfc7
Bob Forcha <bobforcha@gmail.com>
2018-10-26 22:46:12 -0400

    Add attribute validation to lamps
     - validate presence of name and ip_address attributes

--- spec/features/lamps/adding_lamp_spec.rb
+++ spec/features/lamps/adding_lamp_spec.rb
@@ -12,4 +12,15 @@ RSpec.feature "Adding a new lamp", js: true do
     expect(page).to have_content lamp.name
     expect(page).to have_content lamp.status
   end
+
+  scenario "with invalid attributes" do
+    visit root_path
+    click_link "Add Lamp"
+    fill_in "Name", with: ""
+    fill_in "IP Address", with: ""
+    click_button "Create Lamp"
+
+    expect(page).to have_content("Lamp not created!")
+    expect(page).to have_button("Create Lamp")
+  end
 end
