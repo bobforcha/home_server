638741c5625c2e90435c2163ef80b8e1ee1aa2cb
Bob Forcha <bobforcha@gmail.com>
2018-10-26 23:39:31 -0400

    Add IP address validation
     - add uniqueness and format validations to lamp model

--- app/models/lamp.rb
+++ app/models/lamp.rb
@@ -1,4 +1,6 @@
+require 'resolv'
+
 class Lamp < ApplicationRecord
-  validates :name, presence: true
-  validates :ip_address, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :ip_address, presence: true, uniqueness: true, format: { with: Resolv::AddressRegex }
 end
